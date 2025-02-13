13a0068991e47f58ff6610e657305d129d9d0955
kang tatang <kang.tatang@yahoo.co.id>
2025-02-13 15:24:40 +0700

    Edit database file

--- main.go
+++ main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"gin-user-management/database"
 	"gin-user-management/handlers"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	// db := database.InitDB()
+	// Load konfigurasi dari .env
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	// Inisialisasi database
 	database.InitDB()
 	r := gin.Default()
 
@@ -22,5 +30,5 @@ func main() {
 	r.PUT("/users/:id", userHandler.UpdateUser)
 	r.DELETE("/users/:id", userHandler.DeleteUser)
 
-	log.Fatal(r.Run(":8080"))
+	log.Fatal(r.Run(os.Getenv("SERVER_PORT")))
 }
